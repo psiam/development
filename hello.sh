d20dad89fe977936e6c48f51f050758f66294ef8
james mowbray <jamesmowbray@jamess-mbp.home>
2017-10-20 14:04:55 +0100

    Added gin o'clock

--- hello.sh
+++ hello.sh
@@ -3,7 +3,9 @@ if [[ ! -z ${1} ]];then
 	myUser=${1}
 fi
 h=`date +%k`
-if [[ $h -lt 12 ]];then
+if [[ $h -eq 12 ]];then
+	/bin/echo "It's gin o'clock, ${myUser}"
+elif [[ $h -lt 12 ]];then
 	/bin/echo "Good morning, ${myUser}"
 elif [[ $h -lt 18 ]];then
 	/bin/echo "Good afternoon, ${myUser}"
