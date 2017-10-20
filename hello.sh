07647c1ff2d5dc2375d0d9bfdaf77382325d7e0e
james mowbray <jamesmowbray@jamess-mbp.home>
2017-10-20 09:54:18 +0100

    Added time of day message

--- hello.sh
+++ hello.sh
@@ -2,4 +2,11 @@ myUser="nobody"
 if [[ ! -z ${1} ]];then
 	myUser=${1}
 fi
-/bin/echo "Hello ${myUser} `date`"
+h=`date +%k`
+if [[ $h -lt 12 ]];then
+	/bin/echo "Good morning, ${myUser}"
+elif [[ $h -lt 18 ]];then
+	/bin/echo "Good afternoon, ${myUser}"
+else
+	/bin/echo "Good evening, ${myUser}"
+fi
